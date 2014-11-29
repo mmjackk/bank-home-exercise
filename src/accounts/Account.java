d016ec144ae7aff94fc47c41d283a30ab1cb1ebf
Jacky Maman <mmjackk@gmail.com>
2014-11-29 20:51:19 +0200

    initial commit

---
+++ src/accounts/Account.java
@@ -0,0 +1,12 @@
+package accounts;
+
+import java.util.HashMap;
+
+import clients.Client;
+
+
+public class Account 
+{
+	private String _number;
+	private HashMap<String , Client> _myClients;
+}
---
+++ src/clients/Client.java
@@ -0,0 +1,17 @@
+package clients;
+
+import java.util.HashMap;
+
+import accounts.Account;
+
+
+public class Client 
+{
+	private String _id;
+	private String _firstName;
+	private String _lastName;
+	private String _adress;
+	private String _phone;
+	private HashMap<String, Account> _myAccounts;
+	
+}
